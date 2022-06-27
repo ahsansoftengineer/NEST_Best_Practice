80736ff8c04be2baffa3a63e2c18e71db700675d
DESKTOP-9HPI9LN\IndusValley-PC-1 <ahsansoftengineer@gmail.com>
2022-06-27 04:54:14 -0700

    TypeORM EntityManager Table Inheritance CRUD WORKS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,75 @@
-import {  Controller } from '@nestjs/common';
+import {  Controller, Get } from '@nestjs/common';
+import { all_entities } from 'entity/all.entity';
+import { Photo } from 'entity/photo.entity';
+import { Post } from 'entity/post.entity';
+import { PrimeEntity } from 'entity/prime.entity';
+import { Question } from 'entity/question.entity';
+import {  createConnection, getManager } from 'typeorm';
 
+createConnection({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'test',
+  entities: all_entities,
+  // synchronize: true,
+  // dropSchema: true,
+  logging: true
+})
 @Controller()
 export class AppController {
-  
+  @Get('/')
+  async getHello() {
+    const entityManager = getManager()
+    
+    // INSERT
+    // const data  = await entityManager.insert(Question, {
+    //   name: 'Ahsan',
+    //   des: "No Description is Provided",
+    //   question: 'Why learning This'
+    // })
+
+    // INSERT SAME TABLE WITH DIFFERENT TYPE
+    // const data  = await entityManager.insert(Post, {
+    //   name: 'Ahsan',
+    //   des: "No Description is Provided",
+    //   post: "My New Post Created yet"
+    // })
+
+    // const data  = await entityManager.insert(Photo, {
+    //   name: 'Ahsan',
+    //   des: "No Description is Provided",
+    //   size: 35
+    // })
+    
+    // SAVE
+    // const question  = new Question()
+    // question.name = "Ahsan"
+    // question.des = 'Yet Des'
+    // question.question = 'No Question'
+    // const data  = await entityManager.save(question)
+
+    // UPDATE
+    // const data = await entityManager.update(Question, 1, { question: 'New Updated Question'})
+
+    // DELETE
+    // const data = await entityManager.delete(Question, 2)
+
+    // FIND ALL
+    // const data = await entityManager.find(Question)
+
+    // FIND  PRIME ENTITY
+    // const data = await entityManager.find(PrimeEntity)
+
+    // FIND one by id return Object
+    // const data = await entityManager.findOneById(PrimeEntity,  3)
+
+    // FIND BY returns Array
+    // const data = await entityManager.findBy(PrimeEntity,  {id: 3})
+
+    const data = await entityManager.findOne(PrimeEntity,  {where: {id: 3}})
+    return data
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from 'app.controller';
-import { all_entities } from 'entity/all.entity';
-import { FeatureModule } from 'feature/feature.module';
 
 @Module({
-  imports: [
-    FeatureModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: all_entities,
-      // entities: [User, Card],
-      // entities: ['./entity/*.ts'],
-      // This how you Registered your Model Classes
-      synchronize: true,
-      dropSchema: true,
-      logging: true
-    }),
-  ],
+  imports: [],
   controllers: [AppController]
-  // providers: [
-  //   {
-  //     provide: 'APP_GUARD',
-  //     useClass: JwtAuthGuard,
-  //   },
-  // ],
 })
 export class AppModule {}
