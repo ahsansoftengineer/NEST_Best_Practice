ee2249b3fd9eecfa3c2a0d5f64a2d421720848a0
DESKTOP-9HPI9LN\IndusValley-PC-1 <ahsansoftengineer@gmail.com>
2022-06-27 05:07:29 -0700

    GET REPOSITORY CRUD IS WORKING

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,7 @@
 import {  Controller, Get } from '@nestjs/common';
 import { all_entities } from 'entity/all.entity';
-import { Photo } from 'entity/photo.entity';
-import { Post } from 'entity/post.entity';
-import { PrimeEntity } from 'entity/prime.entity';
 import { Question } from 'entity/question.entity';
-import {  createConnection, getManager } from 'typeorm';
+import {  createConnection, getRepository } from 'typeorm';
 
 createConnection({
   type: 'mysql',
@@ -22,54 +19,37 @@ createConnection({
 export class AppController {
   @Get('/')
   async getHello() {
-    const entityManager = getManager()
+    const em = getRepository(Question) // entityManager
     
     // INSERT
-    // const data  = await entityManager.insert(Question, {
+    // const data  = await em.insert({
     //   name: 'Ahsan',
     //   des: "No Description is Provided",
     //   question: 'Why learning This'
     // })
-
-    // INSERT SAME TABLE WITH DIFFERENT TYPE
-    // const data  = await entityManager.insert(Post, {
-    //   name: 'Ahsan',
-    //   des: "No Description is Provided",
-    //   post: "My New Post Created yet"
-    // })
-
-    // const data  = await entityManager.insert(Photo, {
-    //   name: 'Ahsan',
-    //   des: "No Description is Provided",
-    //   size: 35
-    // })
-    
     // SAVE
-    // const question  = new Question()
-    // question.name = "Ahsan"
-    // question.des = 'Yet Des'
-    // question.question = 'No Question'
-    // const data  = await entityManager.save(question)
+    // const data  = await em.save({
+    //   name: "Furqan",
+    //   des: 'DESK',
+    //   question: "Majid is who"
+    // })
 
     // UPDATE
-    // const data = await entityManager.update(Question, 1, { question: 'New Updated Question'})
+    // const data = await em.update(3, { question: 'New New Updated Question'})
 
     // DELETE
-    // const data = await entityManager.delete(Question, 2)
+    // const data = await em.delete(6)
 
     // FIND ALL
-    // const data = await entityManager.find(Question)
-
-    // FIND  PRIME ENTITY
-    // const data = await entityManager.find(PrimeEntity)
+    // const data = await em.find()
 
     // FIND one by id return Object
-    // const data = await entityManager.findOneById(PrimeEntity,  3)
+    // const data = await em.findOneById(3)
 
     // FIND BY returns Array
-    // const data = await entityManager.findBy(PrimeEntity,  {id: 3})
+    // const data = await em.findBy({id: 3})
 
-    const data = await entityManager.findOne(PrimeEntity,  {where: {id: 3}})
+    const data = await em.findOne({where: {id: 1}})
     return data
   }
 }
