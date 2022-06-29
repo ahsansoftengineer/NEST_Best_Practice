8c523dee3422b5dc0598c3305cec21e8687fa31b
DESKTOP-9HPI9LN\IndusValley-PC-1 <ahsansoftengineer@gmail.com>
2022-06-29 09:37:43 -0700

    SWAGGER INTEGERATED

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe())
+  const config = new DocumentBuilder()
+    .setTitle('School Management System (AHSAN)')
+    .setDescription('This application has five major roles (Admin, Headmaster, Teacher, Parents, Student)')
+    .setVersion('1.0.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/', app, document)
   await app.listen(3000);
   if (module.hot) {
     module.hot.accept();
