f89a718ff2e6c7aad52996fcba1d368ce301e542
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2022-07-09 10:29:37 +0500

    CODE FOLDER STRUCTURE

--- src/core/base/BaseUpdateDto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsNumber, Length } from "class-validator";
-import { CreateDto } from "./BaseCreateDto";
-
-export class UpdateDto extends CreateDto  {
-  @ApiProperty()
-  @IsNumber()
-  id: number
-}
