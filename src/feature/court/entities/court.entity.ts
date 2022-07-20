e595f78c4d7d95d27d5e01b49aaeaf4527de0f32
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2022-07-20 07:09:43 -0700

    WORKING

---
+++ src/feature/court/entities/court.entity.ts
@@ -0,0 +1,6 @@
+import { BetaModel } from "core/entities";
+import { Entity } from "typeorm";
+
+
+@Entity()
+export class Court extends BetaModel  {}
---
+++ src/feature/specialization/entities/specialization.entity.ts
@@ -0,0 +1,5 @@
+import { BetaModel } from "core/entities";
+import { Entity } from "typeorm";
+
+@Entity()
+export class Specialization extends BetaModel{}
