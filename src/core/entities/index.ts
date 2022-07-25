08db74f4572be536dcb7f30d7325fe127024fd22
Muhammad Ahsan <ahsan.moin@indusvalley.io>
2022-07-25 14:47:41 +0500

    CENTRALIZING CODE

--- src/core/entities/index.ts
+++ src/core/entities/index.ts
@@ -1,12 +1,14 @@
 export * from './alpha-model';
 export * from './beta-model';
 
-export * from './casez.entity';
-export * from './news.entity';
+export * from './appoinment.entity';
 export * from './book.entity';
+export * from './casez.entity';
 export * from './city.entity';
 export * from './court.entity';
+export * from './news.entity';
 export * from './specialization.entity';
+export * from './task.entity';
 
 export * from './user.entity';
 export * from './lawyer.entity';
--- src/core/shared/service/repo.service.ts
+++ src/core/shared/service/repo.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Court, Lawyer, Specialization, User } from "core/entities";
+import { Court, Lawyer, Specialization, User, Appoinment, LawyerTeam, Book, City, News, Task, Casez } from "core/entities";
 import { Repository } from "typeorm";
 
 @Injectable()
 export class RepoService {
   constructor(
+    @InjectRepository(Appoinment) public appointment: Repository<Appoinment>,
+    @InjectRepository(Book) public book: Repository<Book>,
+    @InjectRepository(Casez) public casez: Repository<Casez>,
+    @InjectRepository(City) public city: Repository<City>,
+    @InjectRepository(Court) public court: Repository<Court>,
+    @InjectRepository(News) public news: Repository<News>,
+    @InjectRepository(Specialization) public specialization: Repository<Specialization>,
+    @InjectRepository(Task) public task: Repository<Task>,
+
+
     @InjectRepository(User) public user: Repository<User>,
     @InjectRepository(Lawyer) public lawyer: Repository<Lawyer>,
-    @InjectRepository(Court) public court: Repository<Court>,
-    @InjectRepository(Specialization) public specialization: Repository<Specialization>,
+    @InjectRepository(LawyerTeam) public lawyerTeam: Repository<LawyerTeam>,
+
+    
   ) {}
 }
