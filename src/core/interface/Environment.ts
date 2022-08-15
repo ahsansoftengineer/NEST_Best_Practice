304e726acb91bce1cf2d901705ae04880ce6ad34
Muhammad Ahsan <ahsan.moin@indusvalley.io>
2022-08-15 07:06:43 -0700

    ENV UPDATED

--- src/core/interface/Environment.ts
+++ src/core/interface/Environment.ts
@@ -7,10 +7,19 @@ export interface Environment {
   DB_PASSWORD: string;
   DB_SYNC: boolean;
   DB_DROP: boolean;
+
+  TYPEORM_SEEDING_FACTORIES:string;
+  TYPEORM_SEEDING_SEEDS:string
+  TYPEORM_MIGERATION:string
+
   JWT_ACCESS_TOKEN: string;
   JWT_REFRESH_TOKEN: string;
   JWT_AT_SECRET: string;
   JWT_RT_SECRET: string;
+  JWT_ACCESS_TOKEN_EXPIRE: string
+  JWT_REFRESH_TOKEN_EXPIRE:string
 
+  MAIL_API_KEY:string
+  MAIL_FROM:string
   [name: string]: any;
 }
